0f250cd5f6f0ffcd0a7c5f280276541cc1b58385
Devansh Desai <devansh@Devanshs-MBP.fios-router.home>
2021-01-18 00:53:21 -0500

    deprecated old school

--- Backend/app.js
+++ Backend/app.js
@@ -2,14 +2,9 @@ const express = require("express");
 const _ = require("lodash");
 const bodyParser = require("body-parser");
 var cors = require("cors");
-const { MongoClient } = require("mongodb");
-const mongoose = require("mongoose");
+const { mongoose } = require('./dbconfig/mongoose');
 const User = require("../model/User");
 const validation = require("./../validation");
-
-const URI = "mongodb://localhost:27017";
-
-// await client.connect();
 
 var app = express();
 app.use(bodyParser.json());
@@ -19,13 +14,6 @@ const port = 3000;
 app.get("/server/status", (req, res) => {
   res.status(200).send("Server OK");
 });
-
-async function listDatabases(client) {
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
-}
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -38,45 +26,13 @@ app.use(function (req, res, next) {
 
 app.listen(port, () => {
   console.log(`Server started at Port No: ${port}`);
-  mongoose
-    .connect(URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(async () => {
-      console.log("Connected to DB");
-      /* New admin creation */
-      let isAdminExist = await User.findOne({ isAdmin: true });
-      if (!isAdminExist) {
-        const hashPassword = await bcrypt.hash("bla123456", saltRounds);
-        new User({
-          EmailID: "admin@gmail.com",
-          password: hashPassword,
-          isAdmin: true,
-          name: "admin",
-          user_id: "admin",
-          group_id: ["Admin"],
-          username: "admin",
-          bio: "Hello admin",
-        })
-          .save()
-          .then(() => {
-            console.log("Admin Created");
-          })
-          .catch((err) => console.log(err));
-      } else {
-        console.log("Admin found: ", isAdminExist);
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 });
 
-route.post("/login", async function (req, res) {
+app.post("/login", async function (req, res) {
   console.log("postLogin in controllers/admin");
   const { email, password } = req.body;
-  const validationResult = validation.loginValidation(req.body);
+  var validationResult = { error : false }
+//   const validationResult = validation.loginValidation(req.body);
   if (validationResult.error) {
     res.status(401).send({ status: "Unauthorized" });
   } else {
