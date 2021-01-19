91e6aaf1003bf65beb6f8be0a9112e4bcf7625c3
Devansh Desai <devansh@Devanshs-MBP.fios-router.home>
2021-01-19 13:50:44 -0500

    cleaning

--- Frontend/src/app/services/auth/auth.service.ts
+++ Frontend/src/app/services/auth/auth.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, map, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 
-import { UserProfile } from '../../model/UserProfile'
+import { UserProfile } from '../../model/UserProfile';
 import { UserSignupInfo } from '../../model/UserSignupInfo';
 
 
@@ -14,31 +13,35 @@ import { UserSignupInfo } from '../../model/UserSignupInfo';
 })
 export class AuthService {
 
-  localLink = "localhost:3000";
+  constructor(private http: HttpClient) { }
+
+  localLink = 'localhost:3000';
   link = this.localLink;
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
-  constructor(private http: HttpClient) { }
+   /** Log a message from Auth Service */
+   private static log(message: string) {
+    console.log(`Auth Service: ${message}`);
+  }
 
-  login(username: String, password: String): Observable<UserProfile> {
-    var body = {
-      'username' : username, 
-      'password' : password
-    }
-    return this.http.post<any>('http://' + this.link + "/login", body)
+  login(username: string, password: string): Observable<UserProfile> {
+    const body = {
+      username, password
+    };
+    return this.http.post<any>('http://' + this.link + '/login', body)
       .pipe(
-        catchError(this.handleError("login"))
+        catchError(this.handleError('login'))
       );
   }
 
-  signup(code: String): Observable<UserSignupInfo> {
-    var body = { 'code' : code }
-    return this.http.post<any>('http://' + this.link + "/signup", body)
+  signup(code: string): Observable<UserSignupInfo> {
+    const body = { code };
+    return this.http.post<any>('http://' + this.link + '/signup', body)
       .pipe(
-        catchError(this.handleError("signup"))
+        catchError(this.handleError('signup'))
       );
   }
 
@@ -55,17 +58,11 @@ export class AuthService {
       console.error(error); // log to console instead
 
       // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
+      AuthService.log(`${operation} failed: ${error.message}`);
 
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
-    
-  }
-
-   /** Log a message from Auth Service */
-   private log(message: string) {
-    console.log(`Auth Service: ${message}`);
   }
 
 
