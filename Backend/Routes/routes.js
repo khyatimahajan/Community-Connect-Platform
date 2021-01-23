8119a6f041a1ec3ef630dd82b8432917e5850097
Devansh Desai <devanshdesai29@gmail.com>
2021-01-23 15:19:23 -0500

    add backend api

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -9,6 +9,7 @@ const router = express.Router();
 const validation = require("./../validation");
 const bcrypt = require("bcryptjs");
 const { urlify } = require("./../utils");
+const upload = require('./middleware/file-upload');
 
 // Get all users
 router.get("/users", async (req, res) => {
@@ -737,5 +738,11 @@ router.post("/create-user", async (req, res) => {
     }
   }
 });
+
+/* Where image is the name of the property sent from angular via the Form Data and the 1 is the max number of files to upload*/
+router.post('/api/v1/upload', upload.array('image', 1), (req, res) => {
+  /* This will be th 8e response sent from the backend to the frontend */
+  res.send({ image: req.file });
+ });
 
 module.exports = router;
---
+++ Backend/middleware/file-uploads.js
@@ -0,0 +1,39 @@
+const aws = require('aws-sdk');
+   const multer = require('multer');
+   const multerS3 = require('multer-s3');
+   const dotenv = require('dotenv');
+   dotenv.config();
+
+   aws.config.update({
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    region: process.env.AWS_REGION, //E.g us-east-1
+   });
+
+   const s3 = new aws.S3();
+
+   /* In case you want to validate your file type */
+   const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+     cb(null, true);
+    } else {
+     cb(new Error('Wrong file type, only upload JPEG and/or PNG !'), 
+     false);
+    }
+   };
+
+   const upload = multer({
+   fileFilter: fileFilter,
+   storage: multerS3({
+    acl: 'public-read',
+    s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    key: function(req, file, cb) {
+      /*I'm using Date.now() to make sure my file has a unique name*/
+      req.file = Date.now() + file.originalname;
+      cb(null, Date.now() + file.originalname);
+     }
+    })
+   });
+
+   module.exports = upload;
