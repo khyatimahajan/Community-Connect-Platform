7976dc266dc4f22114738cbb08c34cc0e7f9d80b
Khyati Mahajan <18370018+khyatimahajan@users.noreply.github.com>
2021-01-23 18:40:33 -0800

    adding indentation for file-uploads

--- Backend/middleware/file-uploads.js
+++ Backend/middleware/file-uploads.js
@@ -15,26 +15,25 @@ const s3 = new aws.S3();
 
 /* In case you want to validate your file type */
 const fileFilter = (req, file, cb) => {
-if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
- cb(null, true);
-} else {
- cb(new Error('Wrong file type, only upload JPEG and/or PNG !'), 
- false);
-}
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+     cb(null, true);
+  } else {
+     cb(new Error('Wrong file type, only upload JPEG and/or PNG !'), false);
+  }
 };
 
 const upload = multer({
-fileFilter: fileFilter,
-storage: multerS3({
-acl: 'public-read',
-s3,
-bucket: process.env.AWS_BUCKET_NAME || 'bruh_bucket',
-key: function(req, file, cb) {
-  /*I'm using Date.now() to make sure my file has a unique name*/
-  req.file = Date.now() + file.originalname;
-  cb(null, Date.now() + file.originalname);
- }
-})
+  fileFilter: fileFilter,
+    storage: multerS3({
+      acl: 'public-read',
+      s3,
+      bucket: process.env.AWS_BUCKET_NAME || 'bruh_bucket',
+      key: function(req, file, cb) {
+        /*I'm using Date.now() to make sure my file has a unique name*/
+        req.file = Date.now() + file.originalname;
+        cb(null, Date.now() + file.originalname);
+       }
+  })
 });
 
 module.exports = upload;
