1c62f392f5981d1fbd59d26e4cd7a1ebbd6d4cdb
Khyati Mahajan <18370018+khyatimahajan@users.noreply.github.com>
2021-01-26 17:39:23 -0800

    fixing

--- Backend/Routes/routes.js
+++ Backend/Routes/routes.js
@@ -696,7 +696,7 @@ router.get("/feeds/:feed_id", async (req, res) => {
 
       var entireCommentsForFeed = await Feeds.find({
         "parent_id": feed_id,
-        "visible_to.groups": { $in: groups }
+        "visible_to.groups": { $in: group }
       }, filters).populate("parent_id", filters);
 
       var response = [];
