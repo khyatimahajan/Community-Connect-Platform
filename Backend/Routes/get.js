6ba93b0f18e3c63345933ea1cd098b11e2935439
Khyati Mahajan <18370018+khyatimahajan@users.noreply.github.com>
2021-02-19 23:35:21 -0500

    updating repost behavior

--- Backend/Routes/get.js
+++ Backend/Routes/get.js
@@ -112,11 +112,8 @@ router.get("/feeds", async (req, res) => {
       .populate("user_id", '_id profile_pic user_handle')
       .limit(100);
 
-      console.log(entireFeeds);
-
       let feeddto = [];
       entireFeeds.forEach(feed => {
-          // console.log(Array.isArray(feed.conversation_visibility_id.visible_to));
           let intersection = feed.conversation_visibility_id.visible_to.filter(x => group.includes(x));
           if (intersection.length > 0) {
             feeddto.push({
@@ -124,13 +121,13 @@ router.get("/feeds", async (req, res) => {
                 author: feed.user_id,
                 body: feed.body,
                 created_at: feed.created_at,
-                like_count: feed.like_count,
-                reply_count: feed.reply_count,
-                repost_count: feed.repost_count,
-                quote_count: feed.quote_count,
-                has_liked: feed.liked_by.includes(user._id)? true : false,
-                has_reposted: feed.reposted_by.includes(user._id)? true : false,
-                replies: feed.replies,
+                like_count: feed.post_type == 'repost'? feed.parent_id.like_count : feed.like_count,
+                reply_count: feed.post_type == 'repost'? feed.parent_id.reply_count : feed.reply_count,
+                repost_count: feed.post_type == 'repost'? feed.parent_id.repost_count : feed.repost_count,
+                quote_count: feed.post_type == 'repost'? feed.parent_id.quote_count : feed.quote_count,
+                has_liked: feed.post_type == 'repost'? feed.parent_id.liked_by.includes(user._id) ? true : false : feed.liked_by.includes(user._id) ? true : false,
+                has_reposted: feed.post_type == 'repost'? feed.parent_id.reposted_by.includes(user._id) ? true : false : feed.reposted_by.includes(user._id) ? true : false,
+                replies: feed.replies, // you don't actually use this info
                 image: feed.image,
                 parent_post: feed.parent_id ? {
                         _id: feed.parent_id._id,
